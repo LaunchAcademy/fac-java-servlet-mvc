e96bf9d57e92f2672fb32c8ae0dbca2ef55f8218
Patrick Whelan <patrickawhelan@gmail.com>
2020-04-07 12:14:26 -0400

    branch to create the controller

--- src/main/java/com/launchacademy/startupideas/IdeasController.java
+++ src/main/java/com/launchacademy/startupideas/IdeasController.java
@@ -25,34 +25,19 @@ public class IdeasController extends HttpServlet {
     EntityManager em = emf.createEntityManager();
 
     // get id from url query
-    String requestedIdea =req.getParameter("id");
+
 
     //if id exists, find idea and render
-    if(requestedIdea != null){
-      String ideaQuery = "SELECT i FROM Idea i WHERE id = :id ";
-      TypedQuery findIdea = em.createQuery(ideaQuery, Idea.class);
-      findIdea.setParameter("id", Long.parseLong(requestedIdea));
-      Idea idea = (Idea) findIdea.getSingleResult();
-      req.setAttribute("idea", idea);
-      System.out.println(idea);
-      RequestDispatcher dispatcher = req.getRequestDispatcher("/views/ideas/show.jsp");
-      dispatcher.forward(req,resp);
-
-      em.close();
-      emf.close();
+    if
       //if no id was given then show the index
     } else {
 
       //Query for our list
-      Query findQuery = em.createQuery("SELECT i FROM Idea i", Idea.class);
-      List<Idea> ideas = findQuery.getResultList();
 
       //Send the list to the JSP
-      req.setAttribute("ideas", ideas);
+
 
       //Render the JSP
-      RequestDispatcher dispatcher = req.getRequestDispatcher("/views/ideas/index.jsp");
-      dispatcher.forward(req, resp);
 
       //Close our connection
       em.close();
